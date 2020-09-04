4672befd360b4e16613e5a28bfd8c1a246cd36c1
Alex P <alexander.peterson89@gmail.com>
2020-09-03 21:23:27 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='django-app-permissions',
     version='0.1.1',
     url='https://github.com/amp89/django-app-permissions',
-    download_url="https://github.com/amp89/django-app-permissions/blob/master/dist/django-app-permissions-0.1.0.tar.gz",
+    download_url="https://github.com/amp89/django-app-permissions/blob/master/dist/django-app-permissions-0.1.1.tar.gz",
     license='MIT',
     description='Automatic app level authentication for django apps using django',
     long_description=open('README.rst', 'r', encoding='utf-8').read(),
